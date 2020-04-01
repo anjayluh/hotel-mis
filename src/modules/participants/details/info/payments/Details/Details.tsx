f97dd726a32ec9c9a31c9887e2908f79ebe08188
Evelyne Namwoyo <evelyne@laboremus.no>
2020-04-01 10:21:52 +0200

    UES-66 Pupulated slideout with payment details

--- src/modules/participants/details/info/payments/Details/Details.tsx
+++ src/modules/participants/details/info/payments/Details/Details.tsx
@@ -1,29 +1,27 @@
 import React, {useEffect, useState} from 'react';
 import {RouteComponentProps, withRouter} from "react-router";
-import {getRouteParam} from "../../../utils/routHelpers";
-import Loading from "../../../components/Loading";
-import Error from "../../../components/Error";
+import {getRouteParam} from "../../../../../../utils/routHelpers";
+import Loading from "../../../../../../components/Loading";
+import Error from "../../../../../../components/Error";
 import {createStyles, Grid, makeStyles, Theme} from "@material-ui/core";
-
-import {IRequestDetails, IWorkflow, trimCaseId} from "../types";
+import {IPaymentDetails, IWorkflow, trimCaseId} from "../../../../types";
 import Typography from "@material-ui/core/Typography";
-import {Flex} from "../../../components/widgets";
+import {Flex} from "../../../../../../components/widgets";
 import Summary from "./Summary";
-import WorkflowView from "./WorkflowView";
-import {put, search} from "../../../utils/ajax";
-import {remoteRoutes} from "../../../data/constants";
+// import WorkflowView from "./WorkflowView";
+import {put, search} from "../../../../../../utils/ajax";
+import {remoteRoutes} from "../../../../../../data/constants";
 import Button from "@material-ui/core/Button";
-import LoaderDialog from "../../../components/LoaderDialog";
+import LoaderDialog from "../../../../../../components/LoaderDialog";
 import {Dispatch} from "redux";
 import {useDispatch, useSelector} from "react-redux";
-import {successColor} from "../../../theme/custom-colors";
-import {renderStatus, renderSubStatus} from "../widgets";
 import Box from "@material-ui/core/Box";
 import Divider from "@material-ui/core/Divider";
-import {fakeRequestDetails} from "../fakeData";
-import {VerificationRequestConstants, IVerificationRequestState} from "../../../data/redux/ninVerification/reducer";
-import {IState} from "../../../data/types";
-import XTextInput from "../../../components/inputs/XTextInput";
+import {fakeRequestDetails} from "../../../../../ninVerification/fakeData";
+import {participantsConstants} from "../../../../../../data/redux/participants/reducer";
+import {IState} from "../../../../../../data/types";
+import XTextInput from "../../../../../../components/inputs/XTextInput";
+import {fakePaymentDetails} from "../../../../fakeData";
 
 
 interface IProps {
@@ -51,9 +49,7 @@ const useWfStyles = makeStyles((theme: Theme) =>
         taskIcon: {
             marginTop: 1
         },
-        successIcon: {
-            color: successColor
-        }
+
     })
 );
 
@@ -71,7 +67,8 @@ const useStyles = makeStyles((theme: Theme) =>
         }
     })
 );
-const requestDetails:IRequestDetails = fakeRequestDetails();
+
+const paymentDetails: IPaymentDetails = fakePaymentDetails();
 
 const Details = (props: IProps) => {
     const dispatch: Dispatch<any> = useDispatch();
@@ -79,7 +76,7 @@ const Details = (props: IProps) => {
     const wfClasses = useWfStyles()
     const classes = useStyles()
     const [blocker, setBlocker] = useState<boolean>(false)
-    const requestData = useSelector((state: IState) => state.verificationRequests.requestDetails);
+    const paymentData = useSelector((state: IState) => state.participants.paymentDetails);
     const [loading, setLoading] = useState<boolean>(false)
 
 
@@ -92,8 +89,8 @@ const Details = (props: IProps) => {
             'filter',
             (resp) => {
                 dispatch({
-                    type: VerificationRequestConstants.RequestDetails,
-                    payload: requestDetails
+                    type: participantsConstants.PaymentsDetailsFetchAll,
+                    payload: paymentDetails
                 })
             },
             undefined,
@@ -107,25 +104,24 @@ const Details = (props: IProps) => {
             props.closeSlideOut()
         }
     }
-    console.log(requestData)
     return (
     <div>
         {
-            requestData &&
+            paymentData &&
             <div className={classes.root} >
 
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <Box display='flex' py={1}>
                             <Box flexGrow={1} pt={1}>
-                                <Typography variant='h5'>Request ID: {requestData ? requestData.referenceNumber: 'loading'}</Typography>
+                                <Typography variant='h5'>Request ID: {paymentData ? paymentData.paymentId: 'loading'}</Typography>
                             </Box>
                         </Box>
                         <Divider/>
                         <Box pt={1}>
                             {
                                 loading ? <Loading/> :
-                                    <Summary data={requestData}/>
+                                    <Summary data={paymentData}/>
                             }
                         </Box>
                     </Grid>
--- src/modules/participants/details/info/payments/Details/Summary.tsx
+++ src/modules/participants/details/info/payments/Details/Summary.tsx
@@ -1,47 +1,40 @@
 import React, {Fragment} from 'react';
 import {Grid} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
-import {ITask, IWorkflow, TaskStatus, IRequestDetails, IRequestDetailsStatus} from "../types";
-import DetailView, {IRec} from "../../../components/DetailView";
-import {printDateTime, printDate} from "../../../utils/dateHelpers";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import LabelIcon from '@material-ui/icons/Label';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from "@material-ui/core/Divider";
-import {errorColor, successColor, warningColor} from "../../../theme/custom-colors";
-import ParticipantLink from "../../../components/links/ParticipantLink";
+import DetailView, {IRec} from "../../../../../../components/DetailView";
+import {printDateTime, printDate} from "../../../../../../utils/dateHelpers";
+import {IPaymentDetails} from "../../../../types";
+
 
 interface IProps {
-    data: IRequestDetails
+    data: IPaymentDetails
 }
 
 const Summary = ({data}: IProps) => {
     const fields: IRec[] = [
         {
-            label: 'Request Date',
-            value: printDateTime(data.requestDate)
+            label: 'Payment Date',
+            value: printDateTime(data.paymentDate)
         },
         {
-            label: 'NIN',
-            value: data.nin
+            label: 'Payment Type',
+            value: data.paymentType
         },
         {
-            label: 'Date of Birth',
-            value: printDate(data.dateOfBirth)
+            label: 'Amount',
+            value: data.amount
+        },
+        {
+            label: 'Date of Entry',
+            value: printDateTime(data.dateOfEntry)
+        },
+        {
+            label: 'EnteredBy',
+            value: data.enteredBy
         },
         {
             label: 'Ref.Number',
             value: data.referenceNumber
-        },
-        {
-            label: 'Initiator',
-            value: data.initiator
-        },
-        {
-            label: 'Participant',
-            value: <ParticipantLink id={data.requestId} name={data.participant}/>
         },
 
     ]
@@ -53,44 +46,11 @@ const Summary = ({data}: IProps) => {
                     <DetailView data={fields}/>
                 </div>
             </Grid>
-            <Grid item xs={12}>
-                <Typography>Request Status</Typography>
-                <List dense>
-                    {
 
-                        data.requestStatus.map(it => (
-                            <Fragment key={it.order}>
-                                <ListItem>
-                                    <ListItemIcon>
-                                        <LabelIcon style={{color: getTaskColor(it)}}/>
-                                    </ListItemIcon>
-                                    <ListItemText
-                                        primary={it.task}
-                                        secondary={it.date && printDateTime(it.date)}
-                                    />
-
-                                </ListItem>
-                                <Divider/>
-                            </Fragment>
-
-                        ))
-                    }
-                </List>
-            </Grid>
         </Grid>
     );
 }
 
-export function getTaskColor(task: IRequestDetailsStatus): any {
-    switch (task.status) {
-        case TaskStatus.Done:
-            return successColor
-        case TaskStatus.Error:
-            return errorColor
-        case TaskStatus.Pending:
-            return warningColor
-    }
-}
 
 
 export default Summary;
