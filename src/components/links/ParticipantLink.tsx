c572551e54cf8720e9d09ed5980c00c30b28cd26
Evelyne Namwoyo <evelyne@laboremus.no>
2020-04-03 09:17:32 +0200

    UES-69 Dispatch data about selected item

--- src/components/links/ParticipantLink.tsx
+++ src/components/links/ParticipantLink.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Link} from "react-router-dom";
-import {localRoutes} from "../../data/constants";
+import {localRoutes, remoteRoutes} from "../../data/constants";
 import {useTheme} from "@material-ui/core";
 import {linkColor} from "../../theme/custom-colors";
+import {useDispatch, useSelector} from "react-redux";
+import {IGeneratedParticipant} from "../../modules/participants/types";
+import {participantsConstants} from "../../data/redux/participants/reducer";
+import {search} from "../../utils/ajax";
+import {fakeParticipantDetails} from "../../modules/participants/fakeData";
 
 interface IProps {
     id: string
@@ -10,6 +15,26 @@ interface IProps {
 }
 
 const ParticipantLink = ({id, name}: IProps) => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        search(
+            remoteRoutes.contacts,
+            'filter',
+            (resp) => {
+                dispatch({
+                    type: participantsConstants.participantsFetchOne,
+                    payload: {...fakeParticipantDetails()}
+                })
+            },
+            undefined,
+            () => {
+                dispatch({
+                    type: participantsConstants.participantsFetchLoading,
+                    payload: false,
+                })
+            })
+    }, [id, dispatch])
     return (
         <Link style={{textDecoration: 'none' ,color:linkColor}} to={`${localRoutes.participants}/${id}`}>{name}</Link>
     );
