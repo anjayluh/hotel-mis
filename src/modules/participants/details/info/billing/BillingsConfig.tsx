fa6a0e4c949ad1840f965334bc89f698085a2cdf
angella <angella@laboremus.no>
2020-04-08 22:02:07 +0300

    UES-64 Adjusted table column sizes

--- src/modules/participants/details/info/billing/BillingsConfig.tsx
+++ src/modules/participants/details/info/billing/BillingsConfig.tsx
@@ -1,61 +1,64 @@
-import {XHeadCell} from "../../../../../components/table/XTableHead";
+import { XHeadCell } from "../../../../../components/table/XTableHead";
 import ParticipantLink from "../../../../../components/links/ParticipantLink";
-import {printDate} from "../../../../../utils/dateHelpers";
-import {printFormattedMoney} from "../../../../../utils/numberHelpers";
+import { printDate } from "../../../../../utils/dateHelpers";
+import { printFormattedMoney } from "../../../../../utils/numberHelpers";
 import React from "react";
 
-
 export const columns: XHeadCell[] = [
-    {
-        name: 'billDate', label: 'Bill Date',
-        render: (value, rec) => <span>{printDate(value)}</span>,
-        cellProps: {
-            style: {
-                width: 70
-            }
-        }
-    },
-    {
-        name: 'billNumber', label: 'Bill Number',
-        cellProps: {
-            style: {
-                width: 70,
-                paddingLeft: 80
-            }
-        }
-    },
-    {
-        name: 'consumption', label: 'Consumption',
-        numeric: true,
-        render: (value, rec) => printFormattedMoney(value),
-        cellProps: {
-            style: {
-                width: 100,
-                textAlign: 'right',
-                paddingRight: 80
-
-            }
-        }
-    },
-    {
-        name: 'rate', label: 'Rate',
-        numeric: true,
-        cellProps: {
-            style: {
-                width: 80,
-                paddingRight: 80
-            }
-        }
-    },
-    {
-        name: 'billAmount', label: 'Bill amount',
-        numeric: true,
-        render: (value, rec) => printFormattedMoney(value),
-        cellProps: {
-            style: {
-                width: 80,
-                paddingRight: 40
-            }
-        }
-    },
+  {
+    name: "billDate",
+    label: "Bill Date",
+    render: (value, rec) => <span>{printDate(value)}</span>,
+    cellProps: {
+      style: {
+        width: 200
+      }
+    }
+  },
+  {
+    name: "billNumber",
+    label: "Bill Number",
+    cellProps: {
+      style: {
+        width: 60,
+        paddingLeft: 80
+      }
+    }
+  },
+  {
+    name: "consumption",
+    label: "Consumption",
+    numeric: true,
+    render: (value, rec) => printFormattedMoney(value),
+    cellProps: {
+      style: {
+        width: 200,
+        textAlign: "right",
+        paddingRight: 80
+      }
+    }
+  },
+  {
+    name: "rate",
+    label: "Rate",
+    numeric: true,
+    cellProps: {
+      style: {
+        width: 200,
+        paddingRight: 80
+      }
+    }
+  },
+  {
+    name: "billAmount",
+    label: "Bill amount",
+    numeric: true,
+    render: (value, rec) => printFormattedMoney(value),
+    cellProps: {
+      style: {
+        width: 150,
+        paddingRight: 40
+      }
+    }
+  }
 ];
--- src/modules/participants/details/info/payments/PaymentsConfig.tsx
+++ src/modules/participants/details/info/payments/PaymentsConfig.tsx
@@ -1,42 +1,47 @@
-import {XHeadCell} from "../../../../../components/table/XTableHead";
-import {printDate} from "../../../../../utils/dateHelpers";
-import {printFormattedMoney} from "../../../../../utils/numberHelpers";
+import { XHeadCell } from "../../../../../components/table/XTableHead";
+import { printDate } from "../../../../../utils/dateHelpers";
+import { printFormattedMoney } from "../../../../../utils/numberHelpers";
 import React from "react";
 
-
 export const columns: XHeadCell[] = [
-    {
-        name: 'paymentDate', label: 'Payment date',
-        render: (value, rec) => <span>{printDate(value)}</span>,
-        cellProps: {
-            style: {
-                width: 60
-            }
-        }
-    },
-    {
-        name: 'paymentType', label: 'Payment type',
-        cellProps: {style: {width: 70}}
-    },
-    {
-        name: 'referenceNumber', label: 'Ref. Number',
-        cellProps: {
-            style: {
-                width: 80,
-            }
-        }
-    },
-    {
-        name: 'amount', label: 'Amount',
-        numeric: true,
-        render: (value, rec) => printFormattedMoney(value),
-        cellProps: {
-            style: {
-                width: 80,
-                paddingRight: 40
-
-            }
-        }
-    },
-
+  {
+    name: "paymentDate",
+    label: "Payment date",
+    render: (value, rec) => <span>{printDate(value)}</span>,
+    cellProps: {
+      style: {
+        width: 200
+      }
+    }
+  },
+  {
+    name: "paymentType",
+    label: "Payment type",
+    cellProps: {
+      style: {
+        width: 200
+      }
+    }
+  },
+  {
+    name: "referenceNumber",
+    label: "Ref. Number",
+    cellProps: {
+      style: {
+        width: 100
+      }
+    }
+  },
+  {
+    name: "amount",
+    label: "Amount",
+    numeric: true,
+    render: (value, rec) => printFormattedMoney(value),
+    cellProps: {
+      style: {
+        width: 80,
+        paddingRight: 40
+      }
+    }
+  }
 ];
