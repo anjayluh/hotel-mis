0ce5d1594ffc801965e54417bb9e87713e32c4d9
Evelyne Namwoyo <evelyne@laboremus.no>
2020-04-16 09:07:23 +0200

    UES-80 Added gateway api

--- src/components/links/ParticipantLink.tsx
+++ src/components/links/ParticipantLink.tsx
@@ -6,8 +6,8 @@ import {linkColor} from "../../theme/custom-colors";
 import {useDispatch, useSelector} from "react-redux";
 import {IGeneratedParticipant} from "../../modules/participants/types";
 import {participantsConstants} from "../../data/redux/participants/reducer";
-import {search} from "../../utils/ajax";
-import {fakeParticipantDetails} from "../../modules/participants/fakeData";
+import {search, get} from "../../utils/ajax";
+// import {fakeParticipantDetails} from "../../modules/participants/fakeData";
 
 interface IProps {
     id: string
@@ -18,26 +18,26 @@ const ParticipantLink = ({id, name}: IProps) => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch({
-            type: participantsConstants.participantsFetchOne,
-            payload: {...fakeParticipantDetails()}
-        })
-        // search(
-        //     remoteRoutes.contacts,
-        //     'filter',
-        //     (resp) => {
-        //         dispatch({
-        //             type: participantsConstants.participantsFetchOne,
-        //             payload: {...fakeParticipantDetails()}
-        //         })
-        //     },
-        //     undefined,
-        //     () => {
-        //         dispatch({
-        //             type: participantsConstants.participantsFetchLoading,
-        //             payload: false,
-        //         })
-        //     })
+        // dispatch({
+        //     type: participantsConstants.participantsFetchOne,
+        //     payload: {...fakeParticipantDetails()}
+        // })
+        get(
+            'https://bou-niv-gatewayservice-test.test001.laboremus.no/api/aggregations/contact-subscriptions/04c8a212-3b79-44c5-6649-08d7daebc579',
+            (resp) => {
+                dispatch({
+                    type: participantsConstants.participantsFetchOne,
+                    payload: {...resp}
+                })
+            },
+            undefined,
+            () => {
+                console.log('failed to access')
+                dispatch({
+                    type: participantsConstants.participantsFetchLoading,
+                    payload: false,
+                })
+            })
     }, [id, dispatch])
     return (
         <Link style={{textDecoration: 'none' ,color:linkColor}} to={`${localRoutes.participants}/${id}`}>{name}</Link>
