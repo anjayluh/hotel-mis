aff5a1e3f4711e5d0c72c1c41c0760e843c414b6
angella <angella@laboremus.no>
2020-04-21 15:27:19 +0300

    Commented out date created in participants filter

--- src/modules/participants/Filter.tsx
+++ src/modules/participants/Filter.tsx
@@ -1,83 +1,86 @@
 import * as React from "react";
-import {useState} from "react";
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 import PDateInput from "../../components/plain-inputs/PDateInput";
-import {toOptions} from "../../components/inputs/inputHelpers";
-import {Box} from "@material-ui/core";
-import TextField from '@material-ui/core/TextField';
+import { toOptions } from "../../components/inputs/inputHelpers";
+import { Box } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
 import PSelectInput from "../../components/plain-inputs/PSelectInput";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 
 interface IProps {
-    onFilter: (data: any) => any
-    loading: boolean
+  onFilter: (data: any) => any;
+  loading: boolean;
 }
 
-const Filter = ({onFilter, loading}: IProps) => {
-    const [data, setData] = useState({
-        name: '',
-        type: '',
-        dateCreated: null
-    })
+const Filter = ({ onFilter, loading }: IProps) => {
+  const [data, setData] = useState({
+    name: "",
+    type: ""
+    // dateCreated: null
+  });
 
-    const participantTypes = ['Commercial Bank', 'Microfinance', 'Forex Bureau']
+  const participantTypes = ["Commercial Bank", "Microfinance", "Forex Bureau"];
 
-    function submitForm(values: any) {
-        onFilter(values);        
-    }
+  function submitForm(values: any) {
+    onFilter(values);
+  }
 
-    function handleChange(event: React.ChangeEvent<any>) {
-        const name = event.target.name
-        const value = event.target.value
-        const newData = {...data, [name]: value}
-        setData({...newData})
-        submitForm(newData)
+  function handleChange(event: React.ChangeEvent<any>) {
+    const name = event.target.name;
+    const value = event.target.value;
+    const newData = { ...data, [name]: value };
+    setData({ ...newData });
+    submitForm(newData);
+  }
+  const handleValueChange = (name: string) => (value: any) => {
+    if (name === "dateCreated") {
+      value = value ? value.toISOString() : value;
     }
-    const handleValueChange = (name: string) => (value: any) => {
-        if (name === 'dateCreated') {
-            value = value ? value.toISOString() : value
-        }
-        const newData = {...data, [name]: value}
-        setData(newData)
-        submitForm(newData)
-    }
-    return <form>
-        <Grid spacing={3} container>
-            <Grid item xs={12}>
-                <Box display='flex' pb={1}>
-                    <Box flexGrow={1}>
-                        <Typography color={'textSecondary'} variant='h5'>Search</Typography>
-                    </Box>
-                </Box>
-                <Divider/>
-            </Grid>
-            <Grid item xs={12}>
-                <TextField
-                    name="name"
-                    value={data['name']}
-                    onChange={handleChange}
-                    label="Participant"
-                    variant="outlined"
-                    fullWidth
-                    size='small'
-                    color="secondary"
-                />
-            </Grid>
-            <Grid item xs={12}>
-                <PSelectInput
-                    name="type"
-                    value={data['type']}
-                    onChange={handleChange}
-                    label="Participant Type"
-                    variant="outlined"
-                    size='small'
-                    color="secondary"
-                    options={toOptions(participantTypes)}
-                />
-            </Grid>
-            <Grid item xs={12}>
+    const newData = { ...data, [name]: value };
+    setData(newData);
+    submitForm(newData);
+  };
+  return (
+    <form>
+      <Grid spacing={3} container>
+        <Grid item xs={12}>
+          <Box display="flex" pb={1}>
+            <Box flexGrow={1}>
+              <Typography color={"textSecondary"} variant="h5">
+                Search
+              </Typography>
+            </Box>
+          </Box>
+          <Divider />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            name="name"
+            value={data["name"]}
+            onChange={handleChange}
+            label="Participant"
+            variant="outlined"
+            fullWidth
+            size="small"
+            color="secondary"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <PSelectInput
+            name="type"
+            value={data["type"]}
+            onChange={handleChange}
+            label="Participant Type"
+            variant="outlined"
+            size="small"
+            color="secondary"
+            options={toOptions(participantTypes)}
+          />
+        </Grid>
+        {/* <Grid item xs={12}>
             <PDateInput
                     name="dateCreated"
                     value={data['dateCreated'] || null}
@@ -86,19 +89,22 @@ const Filter = ({onFilter, loading}: IProps) => {
                     variant="inline"
                     inputVariant='outlined'
                 />
-            </Grid>
-            <Grid item xs={12}>
-                <Box display="flex" flexDirection="row" >
-                    <Button
-                        disabled={loading}
-                        variant="contained"
-                        color="secondary"
-                        onClick={submitForm}>Apply Filter</Button>
-                </Box>
-            </Grid>
+            </Grid> */}
+        <Grid item xs={12}>
+          <Box display="flex" flexDirection="row">
+            <Button
+              disabled={loading}
+              variant="contained"
+              color="secondary"
+              onClick={submitForm}
+            >
+              Apply Filter
+            </Button>
+          </Box>
         </Grid>
+      </Grid>
     </form>
+  );
+};
 
-}
-
-export default Filter
+export default Filter;
