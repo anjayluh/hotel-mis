46c472d5fb57b016dd03213bfd760e0c45b71cb8
Evelyne Namwoyo <evelyne@laboremus.no>
2020-07-16 09:43:20 +0200

    UES-244 -Updated login page.

--- src/modules/login/LoginSimple.tsx
+++ src/modules/login/LoginSimple.tsx
@@ -14,58 +14,58 @@ function Login() {
     const classes = useLoginStyles();
     const [loading,setLoading]=useState(false)
     return (
-        <main className={classes.main}>
-            <CssBaseline/>
-          <Paper className={classes.paper}>
-            <Grid container spacing={3}>
-              <Grid item xs={6} style={{ marginTop: "auto",  marginBottom: "auto"  }}>
-                <Box display="flex" justifyContent="center" >
-                  <img src={coatOfArms} alt={ "Coat of arms of Uganda" } className={classes.coatOfArms}/>
-                </Box>
-              </Grid>
-              <Grid item xs={1} style={{ padding: 0 }}>
-                <Divider orientation={'vertical'}/>
-              </Grid>
-              <Grid item xs={5} style={{ padding: 0 }}>
-                <Box pb={1}>
-                  <Box flexGrow={1} mb={2}>
-                    <Typography  variant="h6">
-                      NATIONAL IDS VERIFICATION AND VALIDATION SYSTEM
-                    </Typography>
-                  </Box>
-                  <Box flexGrow={1}>
-                    <Typography variant={"body2"} style={{ fontSize: 11 }}>
-                      Authentication of current and prospective customer information against records maintained by NIRA
-                    </Typography>
-                  </Box>
-                  <Box flexGrow={1}>
-                    <form className={classes.form}>
-                      <Button
-                        onClick={(event)=> {
-                          event.preventDefault();
-                          setLoading(true)
-                          authService.login().finally(()=>{
-                            setLoading(false)
-                          })
-                        }}
-                        disabled={loading}
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="secondary"
-                        size={"large"}
-                        className={classes.submit}
-                      >
-                        Authenticate
-                      </Button>
-                    </form>
-                  </Box>
-                </Box>
-              </Grid>
-
+      <main className={classes.main}>
+        <CssBaseline/>
+        <Paper className={classes.paper}>
+          <Grid container spacing={3}>
+            <Grid item xs={5} style={{ marginTop: "auto",  marginBottom: "auto", paddingRight: 45 }}>
+              <Box display="flex" justifyContent="center" >
+                <img src={coatOfArms} alt={ "Coat of arms of Uganda" } className={classes.coatOfArms}/>
+              </Box>
             </Grid>
-          </Paper>
-        </main>
+            <Grid item xs={1} style={{ padding: 0 }}>
+              <Divider orientation={'vertical'}/>
+            </Grid>
+            <Grid item xs={6} style={{ padding: 0, paddingRight: 13 }}>
+              <Box pb={1}>
+                <Box flexGrow={1} mb={2} pt={1}>
+                  <Typography  variant="h6">
+                    NATIONAL IDS VERIFICATION AND VALIDATION SYSTEM
+                  </Typography>
+                </Box>
+                <Box flexGrow={1}>
+                  <Typography variant={"body2"} style={{ paddingRight: 16 }}>
+                    Authentication of current and prospective customer information against records maintained by NIRA
+                  </Typography>
+                </Box>
+                <Box flexGrow={1} pb={1}>
+                  <form className={classes.form}>
+                    <Button
+                      onClick={(event)=> {
+                        event.preventDefault();
+                        setLoading(true)
+                        authService.login().finally(()=>{
+                          setLoading(false)
+                        })
+                      }}
+                      disabled={loading}
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="secondary"
+                      size={"large"}
+                      className={classes.submit}
+                    >
+                      Authenticate
+                    </Button>
+                  </form>
+                </Box>
+              </Box>
+            </Grid>
+
+          </Grid>
+        </Paper>
+      </main>
     );
 }
 
